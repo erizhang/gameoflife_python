aa5727c418f46213086c7a25b0b9ace27633044d
Eric Zhang <zhang.lyuan@gmail.com>
2013-10-29 17:59:21 +0800

    pass the aliveNeighbourCount unit testing case

--- gamePackage/game.py
+++ gamePackage/game.py
@@ -1,9 +1,23 @@
 import sys
 
 class Game:
-    def __init__(self, city):
+    def __init__(self, size, city):
+        self.m_size = size
         self.m_city = city
 
+    def __isAlive(self, col, row):
+        pos = row * self.m_size + col
+        if self.m_city[pos] == '1':
+            return True
+        return False
+
     def aliveNeighbourCount(self, col, row):
-        return 0
+        offset = [[-1, -1], [-1, 0], [-1, 1], [0, -1], \
+                  [0, 1], [1, -1], [1, 0], [1, 1]]
+        count = 0
+        for i in range(8):
+            if self.__isAlive(col + offset[i][1], row + offset[i][0]) is True:
+                count += 1
+        return count
         
+    
