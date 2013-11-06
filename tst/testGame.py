f002ef4e53cdbb849957ee84adb39449853f1129
Eric Zhang <zhang.lyuan@gmail.com>
2013-11-06 11:36:35 +0800

    fix the crowded to die case

--- tst/testGame.py
+++ tst/testGame.py
@@ -38,9 +38,9 @@ class TestNextGeneration(unittest.TestCase):
         self.assertEqual("000000000", next_gen, "Along to die is failure")
 
     def testNextGenerationCrowdedToDie(self):
-        g = Game(3, "010111010")
+        g = Game(3, "111111111")
         next_gen = g.nextGeneration()
-        self.assertEqual("010101010", next_gen, "Crowded to die is failure")
+        self.assertEqual("101000101", next_gen, "Crowded to die is failure")
 
     def testNextGenerationStableAlive(self):
         g = Game(3, "110110000")
